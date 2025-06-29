97c01903b8b1230474d898b26898c169c81d0a5e
kendylopes <kendylopes@gmail.com>
2025-06-29 11:23:13 -0400

    feat: cria página Dashboard
    - Adiciona layout básico e responsivo para o painel do usuário.

---
+++ inteface/src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+const Dashboard = () => {
+  return (
+    <div className="container-app min-h-screen flex items-center justify-center bg-gray-300 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Dashboard</h2>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          Bem-vindo ao seu painel de controle!
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Dashboard;
