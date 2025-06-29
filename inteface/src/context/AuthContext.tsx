17b42482ed39d5049d0890bb4af83c4994788e7f
kendylopes <kendylopes@gmail.com>
2025-06-29 10:42:58 -0400

    fix: corrige retorno do hook useAuth e integra login com Google
    - Garante que o hook useAuth retorna corretamente o contexto de autenticação.
    - Permite uso de signWithGoogle e signOut na página de login.
    - Mantém integração com Firebase Auth e GoogleAuthProvider.

---
+++ inteface/src/context/AuthContext.tsx
@@ -0,0 +1,100 @@
+import {
+	createContext,
+	useContext,
+	useEffect,
+	useState,
+	type ReactNode,
+} from "react";
+import type { AuthState } from "../types/auth";
+import {
+	signInWithPopup,
+	onAuthStateChanged,
+	signOut as firebaseSignOut,
+} from "firebase/auth";
+import { firebaseAuth, googleAuthProvider } from "../config/firebase";
+
+interface AuthContextProps {
+	authState: AuthState;
+	signWithGoogle: () => Promise<void>;
+	signOut: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+	const [authState, setAuthState] = useState<AuthState>({
+		user: null,
+		error: null,
+		loading: false,
+	});
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(
+			firebaseAuth,
+			(user) => {
+				if (user) {
+					setAuthState({
+						user: {
+							uid: user.uid,
+							email: user.email,
+							displayName: user.displayName,
+							photoURL: user.photoURL,
+						},
+						error: null,
+						loading: false,
+					});
+				} else {
+					setAuthState({ user: null, error: null, loading: false });
+				}
+			},
+			(error) => {
+				console.error("Erro ao verificar autenticação:");
+				setAuthState({ user: null, error: error.message, loading: false });
+			},
+		);
+
+		return () => unsubscribe();
+	}, []);
+
+	const signWithGoogle = async (): Promise<void> => {
+		setAuthState((prev) => ({ ...prev, loading: true }));
+
+		try {
+			await signInWithPopup(firebaseAuth, googleAuthProvider);
+		} catch (err) {
+			const message =
+				err instanceof Error ? err.message : "Erro ao tentar logar";
+
+			setAuthState((prev) => ({ ...prev, loading: false, error: message }));
+		}
+	};
+
+	const signOut = async (): Promise<void> => {
+		setAuthState((prev) => ({ ...prev, loading: true }));
+
+		try {
+			await firebaseSignOut(firebaseAuth);
+		} catch (err) {
+			const message =
+				err instanceof Error ? err.message : "Erro ao tentar logar";
+
+			setAuthState((prev) => ({ ...prev, loading: false, error: message }));
+		}
+	};
+
+	return (
+		<AuthContext.Provider value={{ authState, signWithGoogle, signOut }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export const useAuth = () => {
+	const context = useContext(AuthContext);
+
+	if (!context) {
+		throw new Error("useAuth deve ser usado de um AuthProvider ");
+	}
+
+	return context;
+};
--- inteface/src/routes/index.tsx
+++ inteface/src/routes/index.tsx
@@ -1,17 +1,22 @@
 import {BrowserRouter, Routes, Route} from 'react-router';
 import Home from '../pages/Home';
 import Login from "../pages/Login"
+import { AuthProvider } from '../context/AuthContext';
 
 const AppRoutes = () =>{
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
 
-        <Route path="*" element={<h2>Página não encontrada</h2>}/>
+      <AuthProvider>
+      
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+
+          <Route path="*" element={<h2>Página não encontrada</h2>}/>
         
-      </Routes>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
