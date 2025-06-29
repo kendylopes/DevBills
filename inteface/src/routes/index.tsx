7ed2ce06e0de1e21bff094a43f6fb07a79274aba
kendylopes <kendylopes@gmail.com>
2025-06-29 11:24:42 -0400

    feat: configura rotas principais com autenticação
    - Adiciona rota para Dashboard.

--- inteface/src/routes/index.tsx
+++ inteface/src/routes/index.tsx
@@ -1,24 +1,23 @@
-import {BrowserRouter, Routes, Route} from 'react-router';
-import Home from '../pages/Home';
-import Login from "../pages/Login"
-import { AuthProvider } from '../context/AuthContext';
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import { AuthProvider } from "../context/AuthContext";
+import Dashboard from "../pages/Dashboard";
 
-const AppRoutes = () =>{
-  return (
-    <BrowserRouter>
+const AppRoutes = () => {
+	return (
+		<BrowserRouter>
+			<AuthProvider>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/dashboard" element={<Dashboard />} />
 
-      <AuthProvider>
-      
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
-
-          <Route path="*" element={<h2>Página não encontrada</h2>}/>
-        
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
-  );
-}
+					<Route path="*" element={<h2>Página não encontrada</h2>} />
+				</Routes>
+			</AuthProvider>
+		</BrowserRouter>
+	);
+};
 
 export default AppRoutes;
