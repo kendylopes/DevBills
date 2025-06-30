2f2702aa7ae59b429a985cad59ac7559a1b97bdb
kendylopes <kendylopes@gmail.com>
2025-06-29 21:43:22 -0400

    feat: adiciona estrutura de rotas e componentes base
    - Configura rotas principais com BrowserRouter, incluindo Home, Login, Dashboard e rota para página não encontrada.
    - Cria componentes Header e Footer básicos.
    - Adiciona componente PrivateRoutes para proteger rotas autenticadas.
    - Implementa AuthProvider para gerenciar autenticação globalmente.

---
+++ inteface/src/routes/PrivateRoutes.tsx
@@ -0,0 +1,15 @@
+
+
+const PrivateRoutes = () => {
+
+  return (
+
+    <div>
+      <h1>Private Routes</h1>
+      {/* Aqui você pode adicionar as rotas privadas que precisam de autenticação */}
+    </div>
+
+  )
+}
+
+export default PrivateRoutes;
--- inteface/src/routes/index.tsx
+++ inteface/src/routes/index.tsx
@@ -3,6 +3,8 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import { AuthProvider } from "../context/AuthContext";
 import Dashboard from "../pages/Dashboard";
+import PrivateRoutes from "./PrivateRoutes";
+import AppLayout from "../layout/AppLayout";
 
 const AppRoutes = () => {
 	return (
@@ -11,7 +13,14 @@ const AppRoutes = () => {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/login" element={<Login />} />
-					<Route path="/dashboard" element={<Dashboard />} />
+
+					<Route element={<PrivateRoutes />}>
+						<Route element={<AppLayout/>}>
+
+							<Route path="/dashboard" element={<Dashboard />} />
+
+						</Route>
+					</Route>
 
 					<Route path="*" element={<h2>Página não encontrada</h2>} />
 				</Routes>
